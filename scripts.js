6d5bceec8b3cb7e3befcf43bdf36fdea6693ecca
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-10-29 23:03:59 +0600

    testing bro testing

---
+++ scripts.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-bitwise */
+// const matchHouses = (num) => (num !== 0 ? 6 * num - (num - 1) : 0);
+
+// const minMax = (arr) => [Math.min(...arr), Math.max(...arr)];
+
+// function timeForMilkAndCookies(date) {
+//     return date.getMonth() === 11 && date.getDate() === 24;
+// }
+
+// function whichIsLarger(f, g) {
+
+// }
+
+// const whichIsLarger = (f, g) => {
+//     return f() > g() ? 'f': g()>f()? 'g': 'neither';
+
+// }
+// const binary = (num) => num.toString(2);
+// const canNest = (arr1, arr2) => {
+//     const minCheck = Math.min(...arr1) > Math.min(...arr2);
+//     const maxCheck = Math.max(...arr1) < Math.max(...arr2);
+//     return minCheck && maxCheck;
+// };
+// console.log(canNest([1, 2, 3, 4], [0, 6]));
+
